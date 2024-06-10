45d6cb53c216d3614e32cdf5700d762b6d87d978
valletivarish <varishvaleti52@gmail.com>
2024-06-10 22:32:08 +0530

    work done till arraylist

---
+++ 02-assignment-app/src/com/monocept/model/Account.java
@@ -0,0 +1,54 @@
+package com.monocept.model;
+
+import com.monocept.exceptions.InsufficientFundsException;
+import com.monocept.exceptions.NegativeAmountException;
+import com.monocept.exceptions.NegativeAmountException;
+public class Account {
+	private String accountName;
+	private long accountNumber;
+	private double accountBalance;
+	public Account(String accountName, long accountNumber, double accountBalance) {
+		super();
+		this.accountName = accountName;
+		this.accountNumber = accountNumber;
+		this.accountBalance = accountBalance;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public long getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	public void deposit(double amount) throws NegativeAmountException{
+		if(amount<0) {
+			throw new NegativeAmountException("Cannot deposit a negative amount.");
+		}
+		this.accountBalance+=amount;
+		System.out.println(amount+" is successfully deposited");
+	    System.out.println("Current Balance : "+this.accountBalance);
+	}
+	public void withdraw(double amount) throws InsufficientFundsException, NegativeAmountException {
+	    if (amount < 0) {
+	        throw new NegativeAmountException("Cannot withdraw a negative amount.");
+	    }
+	    if (this.getAccountBalance() < amount) {
+	        throw new InsufficientFundsException();
+	    }
+	    this.accountBalance -= amount;
+	    System.out.println(amount+" is successfully withdrawn");
+	    System.out.println("Current Balance : "+this.accountBalance);
+	}
+
+}
